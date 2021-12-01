e445e7e0e35635296df04e03375ec4f3f8f054ad
Sebastian Nilsson <sebbehebbe@gmail.com>
2021-12-02 00:07:33 +0100

    Added run script

---
+++ run_solution.sh
@@ -0,0 +1,8 @@
+DAY=$1
+echo "🎅🏼 Running solution of day $DAY 🎅🏼\n"
+echo "🎄🎄🎄🎄 Part 1"
+gcc -o $DAY/day${DAY}_part1.o $DAY/day${DAY}_part1.c && $DAY/day${DAY}_part1.o
+
+echo "\n"
+echo "🎄🎄🎄🎄 Part 2"
+gcc -o $DAY/day${DAY}_part2.o $DAY/day${DAY}_part2.c && $DAY/day${DAY}_part2.o
